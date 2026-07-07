bf7d6c0ee89bc92c41187f561003cfb6d69dc7f5
ledinhvan9x <99377269+ledinhvan9x@users.noreply.github.com>
2026-07-07 21:44:38 +0700

    Create breadth-first-values.js

---
+++ breadth-first-values.js
@@ -0,0 +1,17 @@
+function breadthFirstValuesIterative(root) {
+  if (root === null) return [];
+
+  const result = [];
+  const queue = [root];
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+
+    result.push(current.val);
+
+    if (current.left) queue.push(current.left);
+    if (current.right) queue.push(current.right);
+  }
+
+  return result;
+}
